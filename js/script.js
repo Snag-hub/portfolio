f9f52e55ea7dddf9056d4c68660c860092b7822a
Snag <nadduanwar.99>
2022-02-07 18:27:17 +0530

    Sab

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var titles=["Front End Developer.","WordPress Developer.","YouTuber.", "Freelancer."];
+var count=0;
+var title=document.getElementById("titleChange");
+setInterval(function(){
+  title.innerHTML=titles[count];
+  count++;
+  if(count==titles.length){
+    count=0;
+  }
+},1000);
+
+var greetings=["नमस्ते,","أسلم ولكم,","Hello,"];
+var count1=0;
+var greeting=document.getElementById("greetings");
+setInterval(function(){
+  greeting.innerHTML=greetings[count1];
+  count1++;
+  if(count1==greetings.length){
+    count1=0;
+  }
+},1000);
